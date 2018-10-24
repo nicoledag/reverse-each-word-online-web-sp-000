9ba00e5498bcc5111803220b473c53a57710ac94
Nicole D <nnardella@gmail.com>
2018-10-24 16:12:09 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,16 @@
+require 'pry'
+
+def reverse_each_word(string)
+
+
+ string = string.split
+
+ reversed_string = []
+
+ string.collect do |word|
+ reversed_string << word.reverse
+ end
+
+ return reversed_string.join(' ')
+
+end
